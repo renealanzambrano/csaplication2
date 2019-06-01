0cc00e1114c3216eae3da7988fff31a88d04ccc4
renealanzambrano <43194910+renealanzambrano@users.noreply.github.com>
2019-06-01 11:37:57 -0500

    segundo deploy

--- csaplication/settings.py
+++ csaplication/settings.py
@@ -102,22 +102,22 @@ WSGI_APPLICATION = 'csaplication.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME':'ejemplo_db',
-    'USER': 'postgres',
-    'PASSWORD':'4815162342',
-    'HOST':'localhost',
-    'PORT':'5432',
+# DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     # }
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     'NAME':'ejemplo_db',
+#     'USER': 'postgres',
+#     'PASSWORD':'4815162342',
+#     'HOST':'localhost',
+#     'PORT':'5432',
 
-    }
+#     }
        
-}
+# }
 
 
 # Password validation
@@ -167,6 +167,7 @@ except ImportError:
     # es como generar una funcion que no contiene nada
     pass 
 if not DEBUG :
+    SECRET_KEY= 'SECRET_KEY'
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
