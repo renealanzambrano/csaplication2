294f18bb7996944d114e47fdd96827a2b7e42080
renealanzambrano <43194910+renealanzambrano@users.noreply.github.com>
2019-06-01 13:15:48 -0500

    cuarto deploy

--- csaplication/settings.py
+++ csaplication/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wf66gqf0+3@@6en)^)y*nrgvlxxg!_efgu)&x+yp$jvp2$bi=*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
